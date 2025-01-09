2796b587e52f415078f70bd97a636ea31d280a42
Semyon <semafonplay@gmail.com>
2025-01-09 23:08:59 +0700

    Accepted submission
    There is a completed task for this problem.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,28 @@
 #include <format>
+#include <ios>
 #include <iostream>
-
 
 using namespace std;
 
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+bool checkTree(TreeNode* root) {
+    auto sumOfChilds = root->left->val + root->right->val;
+    return sumOfChilds == root->val;
+}
+
 int main() {
-    constexpr auto greetingsTo = "World";
+    auto left = new TreeNode(4);
+    auto right = new TreeNode(6);
+    auto root = new TreeNode(10, left, right);
 
-    cout << format("Hello, {}!\n", greetingsTo);
-
-    return 0;
+    cout << std::boolalpha << checkTree(root) << "\n";
 }
