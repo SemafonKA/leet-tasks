5ed29ccd3d034c881b0274792ff277e7ca36069a
Semyon <semafonplay@gmail.com>
2025-01-10 00:12:40 +0700

    accepted

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
-#include <format>
-#include <iostream>
 
+#include <iostream>
 
 using namespace std;
 
-int main() {
-    constexpr auto greetingsTo = "World";
+int numberOfSteps(int num) {
+    if (num <= 0) return 0;
 
-    cout << format("Hello, {}!\n", greetingsTo);
+    int steps = 0;
+    while (num > 0) {
+        steps += (num & 1) + 1;
+        num >>= 1;
+    }
+
+    return steps - 1;
+}
+
+int main() {
+    int num = 8;
+    cout << "Number of steps to reduce " << num << " to zero: " << numberOfSteps(num) << endl;
 
     return 0;
 }
