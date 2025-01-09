3311094a8aee0abc0e5bd4fc32eab3534de56649
Semyon <semafonplay@gmail.com>
2025-01-09 23:59:01 +0700

    accepted

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,37 @@
+#include <algorithm>
 #include <format>
 #include <iostream>
-
+#include <iterator>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    constexpr auto greetingsTo = "World";
+vector<string> fizzBuzz(int n) {
+    vector<string> result(n);
 
-    cout << format("Hello, {}!\n", greetingsTo);
+    for (size_t i = 1; i <= n; i++) {
+        string str = "";
+        if (i % 3 == 0) {
+            str += "Fizz";
+        }
+        if (i % 5 == 0) {
+            str += "Buzz";
+        }
+        if (str.empty()) {
+            str = to_string(i);
+        }
+        result.at(i - 1) = str;
+    }
+
+    return result;
+}
+
+int main() {
+    const int size = 15;
+    auto res = fizzBuzz(size);
+
+    ranges::copy(res, ostream_iterator<string>(cout, ", "));
 
     return 0;
 }
