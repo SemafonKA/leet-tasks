df6a826d0c3a93d89b6479f0dd9611a024fe3066
Semyon <semafonplay@gmail.com>
2025-01-10 00:22:48 +0700

    accepted

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
-#include <format>
+#include <array>
 #include <iostream>
-
 
 using namespace std;
 
-int main() {
-    constexpr auto greetingsTo = "World";
+bool canConstruct(string ransomNote, string magazine) {
+    array<int, 256> chars{0};
+    for (const char c : magazine) {
+        chars.at(c + 128) += 1;
+    }
+    for (const char c : ransomNote) {
+        if (--chars.at(c + 128) < 0) {
+            return false;
+        }
+    }
 
-    cout << format("Hello, {}!\n", greetingsTo);
+    return true;
+}
+
+int main() {
+    string ransom = "aa";
+    string magazine = "aab";
+    cout << boolalpha << canConstruct(ransom, magazine);
 
     return 0;
 }
