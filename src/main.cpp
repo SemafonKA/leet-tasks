93d6d26a07239a764530329a770256cdf18b0ea4
Semyon <semafonplay@gmail.com>
2025-01-09 23:44:58 +0700

    accepted

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,36 @@
-#include <format>
-#include <iostream>
 
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
+int maximumWealth(vector<vector<int>>& accounts) {
+    int max = 0;
+
+    size_t customersCount = accounts.size();
+    size_t banksCount = accounts.at(0).size();
+
+    for (size_t i = 0; i < customersCount; i++) {
+        int sum = 0;
+        auto& account = accounts[i];
+        for (size_t j = 0; j < banksCount; j++) {
+            sum += account[j];
+        }
+
+        max = std::max(max, sum);
+    }
+
+    return max;
+}
+
 int main() {
-    constexpr auto greetingsTo = "World";
+    vector<vector<int>> accounts = {
+        {1, 5},
+        {7, 3},
+        {3, 5},
+    };
 
-    cout << format("Hello, {}!\n", greetingsTo);
-
+    auto maxWealth = maximumWealth(accounts);
+    cout << "Maximum wealth: " << maxWealth << "\n";
     return 0;
 }
