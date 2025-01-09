8aa9e753ee8af06c919d8b391a2224a668abe3bd
Semyon <semafonplay@gmail.com>
2025-01-09 23:33:20 +0700

    Accepted

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
-#include <format>
+#include <algorithm>
 #include <iostream>
-
+#include <iterator>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    constexpr auto greetingsTo = "World";
+vector<int> runningSum(vector<int>& nums) {
+    vector<int> res(nums.size());
 
-    cout << format("Hello, {}!\n", greetingsTo);
+    res[0] = nums[0];
+    for (size_t i = 1; i < res.size(); i++) {
+        res[i] = res[i - 1] + nums[i];
+    }
+    return res;
+}
+
+int main() {
+    vector<int> inp = {1, 2, 3, 4};
+    auto out = runningSum(inp);
+
+    ranges::copy(out, std::ostream_iterator<int>(std::cout, " "));
 
     return 0;
 }
