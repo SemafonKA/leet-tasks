33f1923859d9e468ebdfe0880c18c24afc878210
Semyon <semafonplay@gmail.com>
2025-01-09 22:53:38 +0700

    Accepted submission

--- README.md
+++ README.md
@@ -11,3 +11,7 @@
 Также, каждая ветка будет иметь описание задачи, описанном в данном файле в пункте, расположенном ниже.
 
 ## Описание задачи
+
+[Leetcode 2235. Add two integers](https://leetcode.com/problems/add-two-integers)
+
+Given two integers `num1` and `num2`, return the **sum** of the two integers.
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,13 @@
 
 using namespace std;
 
-int main() {
-  constexpr auto greetingsTo = "World";
 
-  cout << format("Hello, {}!\n", greetingsTo);
+int sum(int num1, int num2) { return num1 + num2; }
+
+int main() {
+    const int a = 5;
+    const int b = 6;
+    cout << format("Sum of {} and {} is {}", a, b, sum(a,b));
 
   return 0;
 }
