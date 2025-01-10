daf6f8d781d44e636fd88a3192972b09058fb8fa
Semyon <semafonplay@gmail.com>
2025-01-10 22:58:53 +0700

    Выбрано другое форматирование

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 #include <format>
 #include <iostream>
 
-
 using namespace std;
 
 int main() {
-    constexpr auto greetingsTo = "World";
+  constexpr auto greetingsTo = "World";
 
-    cout << format("Hello, {}!\n", greetingsTo);
+  cout << format("Hello, {}!\n", greetingsTo);
 
-    return 0;
+  return 0;
 }
