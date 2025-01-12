d1af4d717f917f5f87f46150232779b135bdf5ca
Semyon <semafonplay@gmail.com>
2025-01-13 00:11:20 +0700

    Принятое с памятью O(1)

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,29 @@
-#include <iostream>
 #include <vector>
 
 using namespace std;
 
 void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-  auto size = n + m;
-  auto res = vector<int>(size);
-  auto i = 0;
-  auto j = 0;
-  for (auto k = 0; k < size; ++k) {
-    if (i == m) {
-      res[k] = nums2[j];
-      ++j;
-    } else if (j == n) {
-      res[k] = nums1[i];
-      ++i;
+  auto i = m - 1;
+  auto j = n - 1;
+  for (int curr = nums1.size() - 1; curr >= 0; --curr) {
+    if (i == -1) {
+      nums1[curr] = nums2[j];
+      --j;
+      continue;
+    }
+    if (j == -1) {
+      nums1[curr] = nums1[i];
+      --i;
+      continue;
+    }
+    if (nums1[i] > nums2[j]) {
+      nums1[curr] = nums1[i];
+      --i;
     } else {
-      if (nums2[j] < nums1[i]) {
-        res[k] = nums2[j];
-        ++j;
-      } else {
-        res[k] = nums1[i];
-        ++i;
-      }
+      nums1[curr] = nums2[j];
+      --j;
     }
   }
-
-  nums1 = std::move(res);
 }
 
 int main() {
