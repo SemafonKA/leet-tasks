3b54b14491e82062a34b7c44d6e8655bb785ffeb
Semyon <semafonplay@gmail.com>
2025-01-13 00:00:15 +0700

    Принятое с памятью O(n+m)

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,36 @@
-#include <format>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+  auto size = n + m;
+  auto res = vector<int>(size);
+  auto i = 0;
+  auto j = 0;
+  for (auto k = 0; k < size; ++k) {
+    if (i == m) {
+      res[k] = nums2[j];
+      ++j;
+    } else if (j == n) {
+      res[k] = nums1[i];
+      ++i;
+    } else {
+      if (nums2[j] < nums1[i]) {
+        res[k] = nums2[j];
+        ++j;
+      } else {
+        res[k] = nums1[i];
+        ++i;
+      }
+    }
+  }
+
+  nums1 = std::move(res);
+}
+
 int main() {
   constexpr auto greetingsTo = "World";
-
-  cout << format("Hello, {}!\n", greetingsTo);
 
   return 0;
 }
