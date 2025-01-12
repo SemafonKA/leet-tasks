14b3410c6944c2c3682ccf515424c9a4f82e9001
Semyon <semafonplay@gmail.com>
2025-01-13 00:25:48 +0700

    Очень ТУПОЕ решение))))

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
-#include <format>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+int removeElement(vector<int>& nums, int val) {
+  int k = nums.size();
+  for (int i = 0; i < k; ++i) {
+    if (nums[i] == val) {
+      nums[i--] = nums[--k];
+    }
+  }
+  return k;
+}
+
 int main() {
   constexpr auto greetingsTo = "World";
-
-  cout << format("Hello, {}!\n", greetingsTo);
 
   return 0;
 }
