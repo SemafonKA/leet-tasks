8b99b0aabb57a928bceab8f77dea16b4c5220b63
Semyon <semafonplay@gmail.com>
2025-01-13 19:38:14 +0700

    Fix formatting

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,12 @@
 
 using namespace std;
 
-
 int sum(int num1, int num2) { return num1 + num2; }
 
 int main() {
-    const int a = 5;
-    const int b = 6;
-    cout << format("Sum of {} and {} is {}", a, b, sum(a,b));
+  const int a = 5;
+  const int b = 6;
+  cout << format("Sum of {} and {} is {}", a, b, sum(a, b));
 
   return 0;
 }
